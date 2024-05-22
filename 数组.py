a9d75cb488e5c68e0aeb556f89e90a1838cc5496
jsgit <2725679016@qq.com>
2024-05-22 11:10:21 +0800

    【Feature】 add commit py 【QNum】 NA 【Related】 NA

---
+++ 数组.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+A = np.array([1,3,3,4,5])
+print(A)
+str1 = "hello"
+str2 = 'world'
+print(str1 + " " + str2)
+
+x = 5
+if x > 0:
+    print("positive")
+elif x < 0:
+    print("negative")
+else:
+    print("zero")
+
+
+for i in range(1,6):
+    print(i)
+for i in range(2,6):
+    print(i)
+
+def my_function(x):
+    return x*2
+output = my_function(2)
+print(output)
+import matplotlib

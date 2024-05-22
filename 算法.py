b20dda31c4a2109e95f557f9a780736baf3b664c
jsgit <2725679016@qq.com>
2024-05-22 09:11:47 +0800

    【Feature】 add 算法 【QNum】 NA 【Related】 NA

--- 算法.py
+++ 算法.py
@@ -1,27 +1,34 @@
-#反转链表
-class Solution {
-public:
-    ListNode* ReverseList(ListNode* pHead) {
-    ListNode *p,*t;
-int q;
-//存链表节点的值
-stack<int> s;
-p=pHead;
-//遍历链表，将链表节点的值压栈
-while(p!=NULL){
-s.push(p->val);
-p=p->next;
-}
-ListNode* ret=new ListNode(0);
-p=ret;
-//从栈中取出节点值重新分配节点并构建新链表
-while(!s.empty()){
-q=s.top();
-s.pop();
-t=new ListNode(q);
-p->next=t;
-p=t;
-}
-return ret->next;
-}
-};
+# #反转链表
+# 复杂度分析：
+# 时间复杂度：O(n)，遍历。
+# 空间复杂度：O(n)，额外分配的链表节点。
+# class Solution {
+    # public:
+        #ListNode* ReverseList(ListNode* pHead) {
+        #ListNode *p,*t;
+        # int q;
+        # //存链表节点的值
+        # stack<int> s;
+        # p=pHead;
+        # //遍历链表，将链表节点的值压栈
+        # while(p!=NULL){
+            # s.push(p->val);
+            # p=p->next;
+        # }
+        # ListNode* ret=new ListNode(0);
+        # p=ret;
+        # //从栈中取出节点值重新分配节点并构建新链表
+        # while(!s.empty()){
+            # q=s.top();
+            # s.pop();
+            # t=new ListNode(q);
+            # p->next=t;
+            # p=t;
+        # }
+    #   return ret->next;
+    # }
+# };
+
+
+
+

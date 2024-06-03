0145f21a60cf17d15186bedc4866690a43cb2e08
jsgit <2725679016@qq.com>
2024-06-03 17:39:32 +0800

    【Feature】 add commit py 【QNum】 NA 【Related】 NA

--- 知识链.py
+++ 知识链.py
@@ -2,3 +2,7 @@ https://subingwen.cn/cpp/protobuf/
 
 matplotlib 安装出现问题解决：https://blog.csdn.net/lck9296/article/details/133714741
 python.exe -m pip install --upgrade matplotlib
+
+无法定位软件包 python-is-python3   https://geek-docs.com/python/python-ask-answer/239_hk_1710800554.html
+
+https://blog.csdn.net/sinat_16643223/article/details/113974083
